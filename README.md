caa2dfa15e35110c390609c4dff07e49466d086e
tjthejuggler <tjthejuggler@gmail.com>
2023-05-12 00:20:58 -0400

    updated to python specific code

---
+++ README.md
@@ -0,0 +1,20 @@
+This program takes a song as input and outputs settings for stable diffusion deforum video creation.
+
+
+
+
+Spleeter is used to separate the music:
+
+@article{spleeter2020,
+  doi = {10.21105/joss.02154},
+  url = {https://doi.org/10.21105/joss.02154},
+  year = {2020},
+  publisher = {The Open Journal},
+  volume = {5},
+  number = {50},
+  pages = {2154},
+  author = {Romain Hennequin and Anis Khlif and Felix Voituret and Manuel Moussallam},
+  title = {Spleeter: a fast and efficient music source separation tool with pre-trained models},
+  journal = {Journal of Open Source Software},
+  note = {Deezer Research}
+}
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import subprocess
+import whisper_timestamped as whisper
+import json
+import pyperclip
+
 
 def do_terminal_command(command):
     try:
@@ -16,11 +20,24 @@ def create_song_folder(song_filename):
     do_terminal_command(command)
 
 def transcribe_vocals(song_filename):
-    command = "whisper ./outputs/"+song_filename+"/vocals.wav --model large"
-    do_terminal_command(command)
+    # command = "whisper --model large"
+    # do_terminal_command(command)
+    audio = whisper.load_audio("./outputs/"+song_filename+"/vocals.wav")
+
+    model = whisper.load_model("large")
+
+    result = whisper.transcribe(model, audio, language="en")
+    
+    print(json.dumps(result, indent = 2, ensure_ascii = False))
+
+    return result
 
 song_filename = "lucy"
 
 create_song_folder(song_filename)
 run_spleeter_command(song_filename)
-transcribe_vocals(song_filename)
+transcription_dict = transcribe_vocals(song_filename)
+
+#save transcription to clipboard with pyperclip
+
+pyperclip.copy(str(transcription_dict))
